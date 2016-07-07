ef13348a114f0b8a0d262f111c9de67e60562a53
Raghavendra <raghavendra_nayak@intuit.com>
2016-07-07 13:01:18 +0530

    changed db

--- model/db.js
+++ model/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_273pwkbl:heroku_273pwkbl@ds015995.mlab.com:15995/heroku_273pwkbl');
+mongoose.connect('mongodb://raghu556:herokumongo@ds015995.mlab.com:15995/heroku_273pwkbl');
