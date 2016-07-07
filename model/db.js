221969ec8a85d8c7e951b1631d8553662ddbceac
Raghavendra <raghavendra_nayak@intuit.com>
2016-07-07 12:27:09 +0530

    make it better

--- model/db.js
+++ model/db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/book_seller_reviews');
+mongoose.connect('mongodb://heroku_273pwkbl:heroku_273pwkbl@ds015995.mlab.com:15995/heroku_273pwkbl');
